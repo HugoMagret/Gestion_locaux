2082325acafaed8dd67a73a4ab59ee1aa351efea
Clément Ousset <clement.ousset03@gmail.com>
2026-04-27 16:01:06 +0200

    fix front

--- front/src/app/components/staff-list/staff-list.ts
+++ front/src/app/components/staff-list/staff-list.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { StaffService } from '../../services/staff.service';
 import { RoomService } from '../../services/room.service';
+import { Room } from '../../models/room.model';
+import { Staff } from '../../models/staff.model';
 import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-staff-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './staff-list.html',
   styleUrls: ['./staff-list.css']
 })
 export class StaffListComponent implements OnInit {
   staffWithRooms: any[] = [];
+  rooms: Room[] = [];
+  newStaff = {
+    first_name: '',
+    last_name: '',
+    room_id: ''
+  };
 
   constructor(
     private staffService: StaffService,
@@ -28,10 +37,20 @@ export class StaffListComponent implements OnInit {
       staff: this.staffService.getStaff(),
       rooms: this.roomService.getRooms()
     }).subscribe(({ staff, rooms }) => {
+      this.rooms = rooms;
       this.staffWithRooms = staff.map(s => ({
         ...s,
         room: rooms.find(r => r.id === s.room_id)
       }));
+    });
+  }
+
+  addStaff(): void {
+    if (!this.newStaff.first_name || !this.newStaff.last_name) return;
+
+    this.staffService.addStaff(new Staff(this.newStaff)).subscribe(() => {
+      this.newStaff = { first_name: '', last_name: '', room_id: '' };
+      this.loadData();
     });
   }
 
