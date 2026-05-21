95c467150552517e655409d7313f05fcd466b9d8
Clément Ousset <clement.ousset03@gmail.com>
2026-05-21 11:46:45 +0200

    fix: implement actual HTTP requests in SocketService for add and delete

--- front/src/app/services/socket.service.ts
+++ front/src/app/services/socket.service.ts
@@ -1,29 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { Socket } from '../models/socket.model';
-import { RoomService } from './room.service';
+import { API_URL } from '../api.config';
 
 @Injectable({ providedIn: 'root' })
 export class SocketService {
-  constructor(private roomService: RoomService) {}
+  constructor(private http: HttpClient) {}
+
+  getAllSockets(): Observable<Socket[]> {
+    return this.http.get<any[]>(`${API_URL}/sockets`).pipe(
+      map(sockets => sockets.map(s => new Socket(s)))
+    );
+  }
 
   getSocketsByRoom(roomId: string): Observable<Socket[]> {
-    return this.roomService.getRooms().pipe(
-      map(rooms => {
-        const room = rooms.find(r => r.id === roomId);
-        return room ? room.sockets : [];
-      })
+    return this.getAllSockets().pipe(
+      map(sockets => sockets.filter(s => s.room_id === roomId))
     );
   }
 
   addSocket(socket: Socket): Observable<Socket> {
-    // Note: Backend doesn't have a direct socket creation endpoint yet.
-    // You might need to update the entire room or add the endpoint.
-    return this.roomService.getRooms().pipe(map(() => socket));
+    return this.http.post<any>(`${API_URL}/sockets`, socket).pipe(
+      map(s => new Socket(s))
+    );
   }
 
   deleteSocket(id: string): Observable<boolean> {
-    return this.roomService.getRooms().pipe(map(() => true));
+    return this.http.delete(`${API_URL}/sockets/${id}`).pipe(
+      map(() => true)
+    );
   }
 }
 
